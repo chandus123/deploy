3d137db71480c93483f6e38a3445c91bf1cfbe29
chandu123 <chandusuresh13@gmail.com>
2018-08-30 22:56:46 +0530

    main

---
+++ app.js
@@ -0,0 +1,47 @@
+// Include the cluster module
+var cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for terminating workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the terminated workers
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+    var AWS = require('aws-sdk');
+    var express = require('express');
+    var bodyParser = require('body-parser');
+
+    AWS.config.region = process.env.REGION
+    var app = express();
+    const path = require('path');
+
+    app.use(express.static(__dirname + '/dist'));
+
+	app.get('/*', function(req,res) {
+	    
+	res.sendFile(path.join(__dirname+'/dist/index.html'));
+    });
+
+
+    var port = process.env.PORT || 3000;
+
+    var server = app.listen(port, function () {
+        console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
+}
